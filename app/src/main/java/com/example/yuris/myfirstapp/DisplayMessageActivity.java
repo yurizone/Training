fe6c7dd07afc1e8efb8956c412ad9288431f9bb5
yurizone <yurizone.yuris@gmail.com>
2016-10-20 14:15:04 +0700

    upload 1

--- app/src/main/java/com/example/yuris/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/yuris/myfirstapp/DisplayMessageActivity.java
@@ -18,7 +18,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
-        //comment 2
+        //comment 3
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
     }
