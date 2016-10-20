84eafd50affe452425ba5494301efcc19420a448
yurizone <yurizone.yuris@gmail.com>
2016-10-20 14:09:37 +0700

    upload 1

---
+++ app/src/main/java/com/example/yuris/myfirstapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.yuris.myfirstapp;
+
+import android.content.Intent;
+import android.support.annotation.IntDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.edit_message);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
